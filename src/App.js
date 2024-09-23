2a201d3a9a7e8d001dc267a42a56f362ecb430ce
rolandorodriguez1229 <61759440+rolandorodriguez1229@users.noreply.github.com>
2024-09-23 10:28:58 -0500

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,13 @@
+javascript
+     import React from 'react';
+     import XMLParser from './XMLParser';
+
+     function App() {
+       return (
+         <div className="App">
+           <XMLParser />
+         </div>
+       );
+     }
+
+     export default App;
